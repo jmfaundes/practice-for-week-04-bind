01a9828f26931c7b6ad82854dcea15cc84cbffc1
jose <jose@LAPTOP-F7CMF4TI.localdomain>
2023-06-21 17:40:00 -0400

    Final Version

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 // Your code here
+const { Employee } = require('./employee');
+
+let employee = new Employee('John Wick', 'Dog Lover');
+
+setTimeout(employee.sayName, 2000);
+setTimeout(employee.sayName.bind(employee), 2000);
+
+setTimeout(employee.sayOccupation, 3000);
+setTimeout(employee.sayOccupation.bind(employee), 3000);
